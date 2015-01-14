ffb0ac77b43712ab0d97d19c2aca7693df68dd53
Alexander <alexander.mykulych@gmail.com>
2015-01-14 15:13:32 +0200

    Dependency Injection. Controller Factory
    Add my ninject Controller factory.

---
+++ Store/Store.WebUI/DependencyInjection/NinjectControllerFactory.cs
@@ -0,0 +1,33 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Store.Domain.Entities;
+
+namespace Store.WebUI.DependencyInjection
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel kernel;
+
+        public NinjectControllerFactory()
+        {
+            kernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController)kernel.Get(controllerType);
+        }
+        private void AddBindings()
+        {
+            kernel.Bind<IProductRepository>().To<ProductRepository>();
+        }
+
+
+        
+    }
+}
