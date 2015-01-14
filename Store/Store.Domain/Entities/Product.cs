5e72d37134f971816f294a5994457ba358db2b8e
Alexander <alexander.mykulych@gmail.com>
2015-01-14 14:47:38 +0200

    Class to work with DB
    Code-first with Entity Framework. Add class EFDbContext.

--- Store/Store.Domain/Entities/Product.cs
+++ Store/Store.Domain/Entities/Product.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Store.Domain.Entities
 {
-    class Product
+    public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsPresence { get; set; }
+        public int Count { get; set; }
+        public byte[] ImageData { get; set; }
+
+        public string ImageMimeType {get; set; }
     }
 }
