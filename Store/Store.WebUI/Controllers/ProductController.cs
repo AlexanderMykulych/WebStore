1daf715e79c9220dee10a93a809303c001d15f69
Alexander <alexander.mykulych@gmail.com>
2015-01-16 11:57:24 +0200

    Add Product ItemsController

--- Store/Store.WebUI/Controllers/ProductController.cs
+++ Store/Store.WebUI/Controllers/ProductController.cs
@@ -9,17 +9,22 @@ namespace Store.WebUI.Controllers
 {
     public class ProductController : Controller
     {
-        //IProductRepository repository;
-        ////
-        //// GET: /Product/
-        //public ProductController(IProductRepository repoParam)
-        //{
-        //    repository = repoParam;
-        //}
-
-        public PartialViewResult Items()
+        IProductRepository repository;
+        //
+        // GET: /Product/
+        public ProductController(IProductRepository repoParam)
         {
-            return PartialView();
+            repository = repoParam;
+        }
+
+        public PartialViewResult Items(string category = "All", int start = 0, int count = -1)
+        {
+
+            return PartialView(repository.Products
+                .Where(x => category == "All" ? x.Category != null : x.Category == category)
+                .OrderBy(x => x.Id)
+                .Skip(start)
+                .Take( count == -1 ? repository.Products.Count() : count));
         }
 
     }
